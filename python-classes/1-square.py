5ffe761848b77d5b204f3c38a01e45d973bc64d7
c-hirwa <c.hirwa@alustudent.com>
2024-11-07 13:18:53 +0200

    Create 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+
+This module is to create a square
+"""
+
+
+class Square:
+
+    """
+
+    This is a class to calculate the square
+    """
+
+    def __init__(self, size):
+        self.__size = size
