29bd69a4efd0a24b09a9b686dabca2ac17defcdc
c-hirwa <c.hirwa@alustudent.com>
2024-11-07 13:19:08 +0200

    Create 2-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+
+This module is to create a square
+"""
+
+
+class Square:
+
+    """
+
+    This is a class to calculate the square
+    """
+
+    def __init__(self, size=0):
+
+        """
+        initiates the constructor with optional size = 0
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
