cd457a41bf6af577dd4caa051a60c987ca9329da
c-hirwa <c.hirwa@alustudent.com>
2024-11-29 17:10:21 +0200

    Update 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-"""this is for requesting url"""
+"""
+Documentation
+Fetches data from the url using
+the urllib module in python
+"""
+
 import urllib.request
 
+url = 'https://intranet.hbtn.io/status'
+if url.startswith('https://'):
+    url = 'https://alu-intranet.hbtn.io/status'
 
-if __name__ == "__main__":
-    """prevention to work when imported"""
-    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
-        content = response.read()
-
-    print("Body response:")
-    print("\t- type: {}".format(type(content)))
-    print("\t- content: {}".format(content))
-    print("\t- utf8 content: {}".format(content.decode('utf-8')))
+if __name__ == '__main__':
+    with urllib.request.urlopen(url) as f:
+        content = f.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
