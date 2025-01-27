213b971ec9ed0e1226b731e46473763c4babc070
c-hirwa <c.hirwa@alustudent.com>
2025-01-27 10:47:51 +0200

    Done

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
-    console.log(line);
-  }
+
+const strs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (const str of strs) {
+  console.log(str);
+}
--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,7 +1,6 @@
+
 #!/usr/bin/node
 function add (a, b) {
-    const c = a + b;
-    console.log(c);
-  }
-  
-  add(Number(process.argv[2]), Number(process.argv[3]));
+  console.log(a + b);
+}
+add(parseInt(process.argv[2]), parseInt(process.argv[3]));
